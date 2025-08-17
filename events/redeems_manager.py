305efc80bd53ea8d3eb0da886fb82287fad0250d
Tyumici <148009727+tyumici@users.noreply.github.com>
2025-08-16 20:29:56 -0400

    Added redeem handler with a test redeem check

---
+++ events/redeems_manager.py
@@ -0,0 +1,12 @@
+from twitchAPI.object.eventsub import ChannelPointsCustomRewardRedemptionAddEvent
+import models.globals
+
+class RedeemService():
+
+    async def handle_redeems(event: ChannelPointsCustomRewardRedemptionAddEvent):
+        if event.event.reward.title == "test":
+            await models.globals._chat_global.send_message(
+                event.event.broadcaster_user_name,
+                f"[{models.globals._BOT_SIGIL}] test confirmed",
+            )
+        print(f"Point Redemption: {event.event.reward}")
