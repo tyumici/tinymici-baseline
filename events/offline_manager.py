f52c01669b740da8706c894097db963803dd1aed
Tyumici <148009727+tyumici@users.noreply.github.com>
2025-08-16 20:29:25 -0400

    Added stream end manager

---
+++ events/offline_manager.py
@@ -0,0 +1,10 @@
+import os
+
+from twitchAPI.object.eventsub import StreamOfflineEvent
+import models.globals
+
+class OfflineManager:
+    
+    async def change_stream_info(info: StreamOfflineEvent):
+        print('Stream Ended')
+        await models.globals._event_sub_handler_twitch.modify_channel_information(models.globals._PRIMARY_ACCOUNT_TWITCH_ID, None, None, {os.getenv("OFFLINE_TITLE")})
